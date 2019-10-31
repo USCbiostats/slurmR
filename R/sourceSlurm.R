edc3f1f7edf1e3da29b63193401d34209c57f279
George G. Vega Yon <g.vegayon@gmail.com>
2019-10-31 10:15:50 -0700

    Improving the slurmr_cmd docs and function (adding alias)

--- R/sourceSlurm.R
+++ R/sourceSlurm.R
@@ -14,7 +14,7 @@
 #' the R script starts with `#!/bin/` or similar, then `#SBATCH` flags will be
 #' read from the R script and added to the Slurm job file.
 #'
-#' @return Whatever [sbatch] returns.
+#' @return In the case of `sourceSlurm`, Whatever [sbatch] returns.
 #' @export
 #' @examples
 #' # In this example we will be sourcing an R script that also has #SBATCH
@@ -123,7 +123,12 @@ sourceSlurm <- function(
 #' @export
 #' @rdname sourceSlurm
 #' @param cmd_path Character scalar. Path (directory) where to put the comand function.
+#' This is usually your home directory.
 #' @param cmd_name Character scalar. Name of the command (of the file).
+#' @param add_alias,bashrc_path Logical scalar and character scalar. When
+#' `add_alias=TRUE` it will modify (or create, if non-existent) the `.bashrc`
+#' file to add an alias of the same name of `cmd_name`. The path to `.bashrc` can be
+#' specified via the `bashrc_path` option.
 #' @details The function `slurmr_cmd` writes a simple command that works as a wrapper
 #' of `sourceSlurm`. In particular, from command line, if the user wants to source an
 #' R script using `sourceSlurm`, we can either:
@@ -138,11 +143,21 @@ sourceSlurm <- function(
 #' $ ./sbatchr path/to/the/script.R
 #' ```
 #'
-#' The main side effect of this function is that it creates a file named `cmd_name`
-#' in the directory specified by `cmd_path`.
+#' And, if you used the option `add_alias = TRUE`, then, after restarting bash,
+#' you can run R scripts with Slurm as follows:
 #'
+#' ```
+#' $ sbatchr path/to/the/script.R
+#' ```
+#'
+#' The main side effect of this function is that it creates a file named `cmd_name`
+#' in the directory specified by `cmd_path`, and, if `add_alias = TRUE`. it will
+#' create (if not found) or modify (if found) the `.bashrc` file adding a line
+#' with an alias. For more information on `.bashrc` see [here](https://superuser.com/questions/49289).
+#'
+#' @return The function `slurmr_cmd` returns `invisible()`.
 #' @rdname sourceSlurm
-slurmr_cmd <- function(cmd_path, cmd_name = "sbatchr") {
+slurmr_cmd <- function(cmd_path, cmd_name = "sbatchr", add_alias = TRUE, bashrc_path = "~/.bashrc") {
 
   fn   <- suppressWarnings(normalizePath(sprintf("%s/%s", cmd_path, cmd_name), ))
   bash <- new_bash(fn)
@@ -151,12 +166,65 @@ slurmr_cmd <- function(cmd_path, cmd_name = "sbatchr") {
 
   system2("chmod", sprintf("u+x %s", fn))
 
+  if (add_alias) {
+
+    # This is the entry
+    l_alias <- sprintf(
+      "# Alias created by the slurmR R package on %1$s\nalias %2$s=\"%3$s/./%2$s\"",
+      as.character(Sys.time()),
+      cmd_name,
+      cmd_path
+    )
+
+    # Checking whether the file exists or not
+    if (file.exists(bashrc_path)) {
+
+      # Reading the lines and setting the exit protocol in case of emergency
+      f_bashrc <- readLines(bashrc_path)
+      fn_bashrc_tmp <- tempfile()
+      writeLines(text = f_bashrc, con = fn_bashrc_tmp)
+      on.exit({
+        if (!exists("all_ok")) {
+          file.copy(from = fn_bashrc_tmp, to = bashrc_path, overwrite = TRUE)
+          message(
+            "Something went wrong during the update of the .bashrc file. ",
+            "The original file has been restored."
+            )
+        }
+      })
+
+      # Does it exists already?
+      loc <- which(grepl(paste0("^alias ", cmd_name), f_bashrc))
+      if (length(loc)) f_bashrc[loc[1]] <- l_alias
+      else f_bashrc <- c(f_bashrc, l_alias)
+
+    } else
+      f_bashrc <- l_alias
+
+    # Re-writing the file
+    writeLines(text = f_bashrc, con = bashrc_path)
+
+  }
+
   message(
     "Success! The file has been written in: \n  ", fn,
+    ifelse(
+      add_alias,
+      paste0(
+        "\nand an alias has been added to:\n",
+        bashrc_path,
+        "\nYou can start using it in command line after sourcing it with ",
+        "`source ", bashrc_path,"` or restarting bash."
+        ),
+      ""
+      ),
     "\nYou can submits jobs from your command line using the following:\n",
-    "  ./", cmd_name, " path/to/rscript/to/run.R\n",
+    ifelse(add_alias, "  ", "  ./"), cmd_name, " path/to/rscript/to/run.R\n",
     "Remember that this is a wrapper of `sourceSlurm`, to the file must start with '#!/bin/sh'."
     )
+
+  all_ok <- TRUE
+  return(invisible())
 
 }
 

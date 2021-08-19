b60b1f3039864b83d8879851e1968443c6063108
George G Vega Yon <g.vegayon@gmail.com>
2021-08-19 16:47:05 -0700

    Final step for checking coverage

--- docker/rcheck.sh
+++ docker/rcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /usr/bin/R CMD check --no-vignettes --no-manual slurmR_*.tar.gz && \
 	rm -rf covr || mkdir covr && \
-	Rscript -e 'pth <- normalizePath("covr");Sys.setenv(SLURMR_TMP_PATH=pth);saveRDS(covr::package_coverage("slurmR", quiet = FALSE, clean = FALSE, install_path = pth), "covr/dat.rds")' && \
-	Rscript -e 'library(covr);readRDS("covr/dat.rds")'
+	Rscript -e 'pth <- "/home/slurmr-test/"; Sys.setenv(SLURMR_TMP_PATH=pth);saveRDS(covr::package_coverage("slurmR", quiet = FALSE, clean = FALSE, install_path = pth), "/home/slurmr-test/covr.rds")' && \
+	Rscript -e 'library(covr); x <- readRDS("/home/slurmr-test/covr.rds"); codecov(coverage=x, token = Sys.getenv("CODECOV_TOKEN"))'

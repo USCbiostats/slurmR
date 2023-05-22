41481e1e1256295ae8e70fc4730554b3bf31f4af
George G. Vega Yon <g.vegayon@gmail.com>
2023-05-22 15:47:02 -0600

    sourceSlurm now picks the correct path to the script (#35)

--- R/sourceSlurm.R
+++ R/sourceSlurm.R
@@ -76,8 +76,8 @@ sourceSlurm <- function(
   if (!is.null(SBATCH$`job-name`) & is.null(job_name))
     job_name <- SBATCH$`job-name`
 
-  file        <- basename(normalizePath(file))
-  script_path <- gsub("\\.(r|R)$", ".\\1.sh", file)
+  file_name   <- basename(normalizePath(file))
+  script_path <- gsub("\\.(r|R)$", ".\\1.sh", file_name)
   script_path <- normalizePath(file.path(tmp_path, script_path))
 
   x <- new_bash(
@@ -93,7 +93,7 @@ sourceSlurm <- function(
   x$append(opts_slurmR$get_preamble())
 
   # Finalizing by setting the R call
-  x$Rscript(file = file, flags = rscript_opt)
+  x$Rscript(file = normalizePath(file), flags = rscript_opt)
 
   # Writing the script
   x$write()
